14c0030f15fa7e7c19836a98ac3ffbe55eeb1fee
duleepa-a <duleepa24@gmail.com>
2024-09-06 00:24:43 +0530

    title linked to home page

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,7 +5,7 @@ import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import './CSS/navBar.css'
 import logo from './CSS/Images/CLOUDRestaurantLogo.png'
 import SwitchButton from './SwitchButton';
-import { NavLink} from 'react-router-dom';
+import { Link, NavLink} from 'react-router-dom';
 
 
 export default function NavBar() {
@@ -45,8 +45,10 @@ export default function NavBar() {
     <div className={`navBarBox ${isFixed ? 'fixed' : ''} ${darkMode ? 'dark' : ''}` }  >
         <div className="navbarLeft">
             <div className='navBarLogo'>
+                <Link to={"/Home"}>
                 {/* <img src={logo} className='logoPic' /> */}
                 <h2 className={`navBarTitle ${isFixed ? 'fixed' : ''} ${darkMode ? 'dark' : ''}` }>ZellMart</h2>
+                </Link>
             </div> 
         </div>
         <div className='searchBox'>
