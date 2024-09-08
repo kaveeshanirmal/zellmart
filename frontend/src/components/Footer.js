46353ed4d4f4c73dee7ae2582932cfc665e5c508
Rishi20020825 <rasheenmohommed25@gmail.com>
2024-09-08 16:06:42 +0530

    Updated footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,31 +1,44 @@
-import React, { useContext} from 'react';
+import React, { useContext } from 'react';
 import CurrentTime from './CurrentTime';
 import './CSS/footer.css'
-import { ThemeContext } from './ThemeContextProvider'; 
+import { ThemeContext } from './ThemeContextProvider';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
   const { darkMode } = useContext(ThemeContext);
+
   return (
     <footer className={`footer ${darkMode ? 'dark' : ''}`}>
-      <br/>
-      <br/>
       <div className="footerContainer">
-        <div className="row">
-          <div className="col-md-6">
-            <p><strong>Address:</strong> Reid Avenue, Colombo 07,Sri Lanka</p> <br/>
-            <Link to="/ContactUs"><strong>Contact Us</strong> </Link> <br/> <br/>
-            <p><strong>Contact Information:</strong> <br/>
-            Phone : 0755555555555<br/>
-            Email : dsjnsjdk@.com
-            </p><br/>
-            <p>&copy; 2024 ZellMart. All rights reserved.</p>
+        <div className="footer-top">
+          <h2>Want to contact us?</h2>
+          <p>feel free to get in touch.</p>
+          <Link to="/ContactUs" className="cta-button">Contact Us</Link>
+        </div>
+        <div className="footer-content">
+          <div className="footer-section">
+            <h3>ZellMart</h3>
           </div>
-          <div className="col-md-6 text-right">
-            <Link to="/AboutUs"><strong>About Us</strong> </Link> <br/> <br/>
-            <p>Designed by RAD OG picco</p>
-            <CurrentTime/>
+          <div className="footer-section">
+            <h3>Contact Information</h3>
+            <p><strong>Address:</strong> Reid Avenue, Colombo 07, Sri Lanka</p>
+            <p><strong>Phone:</strong> 0755555555555</p>
+            <p><strong>Email:</strong> Radpicco@gmail.com</p>
           </div>
+          <div className="footer-section">
+            <h3>Quick Links</h3>
+            <ul>
+              <li><Link to="/AboutUs">About Us</Link></li>
+              <li><Link to="/ContactUs">Contact Us</Link></li>
+            </ul>
+          </div>
+          <div className="footer-section">
+            <h3>Time</h3>
+            <CurrentTime />
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p>&copy; 2024 ZellMart. All rights reserved. | Designed by RAD OG picco</p>
         </div>
       </div>
     </footer>
